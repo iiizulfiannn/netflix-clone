5e74ae97f0be6844d5b20f1eb9cd504275386aec
zul <iiizulfiannn@gmail.com>
2025-08-23 19:11:49 +0700

    feat: add bottom navigation

--- app/src/main/java/com/luckyfriday/netflixclone/presentation/MainActivity.kt
+++ app/src/main/java/com/luckyfriday/netflixclone/presentation/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.luckyfriday.netflixclone
+package com.luckyfriday.netflixclone.presentation
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.luckyfriday.netflixclone.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
